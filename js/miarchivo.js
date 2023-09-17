ac2200d0da2c24107f8c336ea4e5ae644f97907c
flora-91 <florencialombardi44@gmail.com>
2023-09-17 15:55:59 -0300

    clases

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -28,46 +28,133 @@ function generar() {
   let recomendacion = "Recomendación de rutina:\n";
 
  
-  switch (objetivos) {
-    case "perder_peso":
-      recomendacion +=
-        "Para perder peso, recomendamos una rutina de ejercicios aeróbicos como correr o nadar, acompañada de una dieta equilibrada.";
-      break;
-    case "ganar_masa_muscular":
-      recomendacion +=
-        "Para ganar masa muscular, recomendamos un programa de entrenamiento de fuerza con levantamiento de pesas y una ingesta adecuada de proteínas.";
-      break;
-    case "ambos":
-      recomendacion +=
-        "Si deseas perder peso y ganar masa muscular al mismo tiempo, te recomendamos un enfoque que combine ejercicios aeróbicos y entrenamiento de fuerza, junto con una dieta controlada.";
-      break;
-    case "adelgazar":
-      recomendacion +=
-        "Para adelgazar, te recomendamos una combinación de ejercicios cardiovasculares y una dieta baja en calorías.";
-      break;
+
+ 
+
+  const objetivosRespuestas={
+    perder_peso: "Para perder peso, recomendamos una rutina de ejercicios aeróbicos como correr o nadar, acompañada de una dieta equilibrada.",
+    
+    ganar_masa_muscular:"Para ganar masa muscular, recomendamos un programa de entrenamiento de fuerza con levantamiento de pesas y una ingesta adecuada de proteínas.",
+    
+    ambos:"Si deseas perder peso y ganar masa muscular al mismo tiempo, te recomendamos un enfoque que combine ejercicios aeróbicos y entrenamiento de fuerza, junto con una dieta controlada.",
+
+    adelgazar:"Para adelgazar, te recomendamos una combinación de ejercicios cardiovasculares y una dieta baja en calorías.",
+    
   }
 
-  recomendacion+= '<br>';
+  const estadoFisicoRespuestas={
+    bueno:"Metele pata",
+
+    regular:"Cagaste",
+
+    malo:"En el horno",
+  }
+
+  recomendacion += objetivosRespuestas[objetivos] + '<br>'+ estadoFisicoRespuestas[estadoFisico];
   
-  switch (estadoFisico) {
-    case "bueno":
-      recomendacion +=
-        "Metele pata";
-      break;
-    case "regular":
-      recomendacion +=
-        "Cagaste";
-      break;
-    case "malo":
-      recomendacion +=
-        "En el horno";
-      break;
+  
+ 
+
+
+// rutinas
+
+class RutinaControlador {
+
+  constructor(nombre, orientado) {
+
+    this.nombre = nombre;
+
+    this.orientado = orientado;
+
+    this.ejercicios = [];
+
   }
 
-  gustos.forEach(gusto => {
-    recomendacion+='<br>';
-    recomendacion+= gusto+' x50mil repeticiones';
-  });
+  agregarEjercicio(nombre, duracionEnMin, repeticiones) {
+
+    this.ejercicios.push({
+
+      nombre: nombre,
+
+      duracionEnMin: duracionEnMin,
+
+      repeticiones: repeticiones,
+
+    });
+
+  }
+
+}
+
+const rutinaBoxeo = new RutinaControlador('Rutina1', 'boxeo');
+
+const rutinaSpinning = new RutinaControlador('Rutina de spinning', 'spinning');
+
+const rutinaFuncional = new RutinaControlador ('Rutina de Funcional', 'funcional');
+
+ 
+
+rutinaBoxeo.agregarEjercicio('flexiones', 2, 10);
+rutinaBoxeo.agregarEjercicio('burpees', 10, 50);
+rutinaSpinning.agregarEjercicio('cardio', 23, 6); 
+rutinaSpinning.agregarEjercicio('cardio', 23, 6); 
+
+
+console.log(rutinaBoxeo);
+
+const rutinas = [rutinaBoxeo, rutinaSpinning, rutinaFuncional];
+
+
+const rutinaSeleccionada = rutinas.find(rutina => rutina.orientado==gustos
+)
+
+let rutinasHTML = ''
+
+    const renderizarEjercicios = (ejercicios) =>{
+
+        let resultHTML = ''
+
+        ejercicios.forEach(ejercicio =>{
+          console.log(ejercicio)
+
+            resultHTML += `
+
+                <div>
+
+                    <h3>${ejercicio.nombre}</h3>
+
+                    <span>Repeticiones: ${ejercicio.repeticiones} x ${ejercicio.duracionEnMin} cada uno</span>
+
+                    <hr>
+
+                </div>
+
+            `
+
+        })
+
+        return resultHTML
+
+    }
+
+    rutinasHTML = `
+
+       <h2>${rutinaSeleccionada.nombre}</h2>
+
+
+        <span>Orientado a: ${rutinaSeleccionada.orientado}</span>
+
+        <div>
+
+            ${renderizarEjercicios(rutinaSeleccionada.ejercicios)}
+
+        <div>
+
+    `
+
+    recomendacion += rutinasHTML
+
+  
 
   
   if (isNaN(edad) || edad <= 0) {
